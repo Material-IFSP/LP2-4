683991ccd1e3fb0bb7d7e9399b9d6d00cb36d671
W4rL0ck1 <renato_boy3@hotmail.com>
2020-12-29 22:07:32 -0300

    Terminado o trampo meu consagra

--- TP3/src/Calculadora.java
+++ TP3/src/Calculadora.java
@@ -3,20 +3,21 @@ import java.awt.event.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
 public class Calculadora extends Frame implements ActionListener {
 
     private static double calculo1 = 0;
     private static double calculo2 = 0;
-    private static String tela1 = "0";
-    private static String tela2 = "0";
-
-    private boolean isValue2 = false;
-
     private String operacao = "";
+
+    // #region Instanciando Objetos
 
     JFrame f = new JFrame("TP3 - CALCULADORA");
     JPanel p1 = new JPanel(null);
+    JPanel p2 = new JPanel(null);
+    JPanel p3 = new JPanel(null);
+    JPanel p4 = new JPanel(null);
+    JPanel p5 = new JPanel(null);
+    JPanel p6 = new JPanel(null);
 
     TextField txtFieldNome = new TextField(5);
     Button btn1 = new Button("1");
@@ -29,67 +30,94 @@ public class Calculadora extends Frame implements ActionListener {
     Button btn8 = new Button("8");
     Button btn9 = new Button("9");
     Button btn0 = new Button("0");
+    Button btnDot = new Button(".");
 
     Button mais = new Button("+");
     Button menos = new Button("-");
     Button dividir = new Button("/");
     Button multi = new Button("x");
-    Button clear = new Button("c");
+    Button clear = new Button("C");
     Button igual = new Button("=");
 
+    // #endregion
+
+    //#region Paineis
     public Calculadora() {
         super();
 
         // NÃO retirar a linha abaixo
         txtFieldNome.disable();
-        txtFieldNome.setText("0");
+        txtFieldNome.setText("");
 
-        p1.setLayout(new FlowLayout(FlowLayout.LEFT));
+        p1.setLayout(new GridLayout(0, 1, 0, 5));
         p1.setSize(200, 300);
         p1.add(txtFieldNome);
 
-        btn0.addActionListener(this);
-        btn1.addActionListener(this);
-        btn2.addActionListener(this);
-        btn3.addActionListener(this);
-        btn4.addActionListener(this);
-        btn5.addActionListener(this);
-        btn6.addActionListener(this);
-        btn7.addActionListener(this);
-        btn8.addActionListener(this);
-        btn9.addActionListener(this);
+        p2.setLayout(new GridLayout(0, 4, 0, 10));
+        p2.setSize(200, 300);
+        p2.add(btn1);
+        p2.add(btn2);
+        p2.add(btn3);
+        p2.add(dividir);
 
-        menos.addActionListener(this);
-        mais.addActionListener(this);
-        multi.addActionListener(this);
-        dividir.addActionListener(this);
-        clear.addActionListener(this);
-        igual.addActionListener(this);
+        p3.setLayout(new GridLayout(0, 4, 0, 10));
+        p3.setSize(200, 300);
+        p3.add(btn4);
+        p3.add(btn5);
+        p3.add(btn6);
+        p3.add(multi);
 
-        p1.add(btn1);
-        p1.add(btn2);
-        p1.add(btn3);
-        p1.add(btn4);
-        p1.add(btn5);
-        p1.add(btn6);
-        p1.add(btn7);
-        p1.add(btn8);
-        p1.add(btn9);
-        p1.add(btn0);
-        p1.add(menos);
-        p1.add(mais);
-        p1.add(multi);
-        p1.add(dividir);
-        p1.add(clear);
-        p1.add(igual);
+        p4.setLayout(new GridLayout(0, 4, 0, 10));
+        p4.setSize(200, 300);
+        p4.add(btn7);
+        p4.add(btn8);
+        p4.add(btn9);
+        p4.add(menos);
 
-        f.add(p1);
+        p5.setLayout(new GridLayout(0, 4, 0, 10));
+        p5.setSize(200, 300);
+        p5.add(btn0);
+        p5.add(igual);
+        p5.add(mais);
+        p5.add(clear);
 
-        f.setLayout(new FlowLayout(FlowLayout.LEFT));
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
-        f.setSize(550, 100);
-    }
+        //#endregion
+
+    //#region ActionListener
+    btn0.addActionListener(this);
+    btn1.addActionListener(this);
+    btn2.addActionListener(this);
+    btn3.addActionListener(this);
+    btn4.addActionListener(this);
+    btn5.addActionListener(this);
+    btn6.addActionListener(this);
+    btn7.addActionListener(this);
+    btn8.addActionListener(this);
+    btn9.addActionListener(this);
+
+    menos.addActionListener(this);
+    mais.addActionListener(this);
+    multi.addActionListener(this);
+    dividir.addActionListener(this);
+    clear.addActionListener(this);
+    igual.addActionListener(this);
+
+    //#endregion
+
+    //#region Frame
+    f.add(p1);
+    f.add(p2);
+    f.add(p3);
+    f.add(p4);
+    f.add(p5);
+
+    // f.setLayout(new FlowLayout(FlowLayout.LEFT));
+    f.setLayout(new GridLayout(5, 1, 0, 5));
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setVisible(true);
+    f.setSize(400, 600);
+    //#endregion
+}
 
     public static void main(String[] args) throws Exception {
         new Calculadora();
@@ -99,153 +127,96 @@ public class Calculadora extends Frame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        //#region Eventos Botoes Numericos
         if (e.getSource() == btn0) {
-            if (isValue2) {
-                calculo1 += 0;
-                tela1 += "0";
-                txtFieldNome.setText(tela1);
-            } else {
-                calculo2 += 0;
-                txtFieldNome.setText(Double.toString(calculo2));
-            }
+            txtFieldNome.setText(txtFieldNome.getText().toString() + "0");
         }
         if (e.getSource() == btn1) {
-            if (isValue2) {
-                calculo1 += 1;
-                tela1 = (tela1 + "1");
-                txtFieldNome.setText(tela1);
-                
-            } else {
-                calculo2 += 1;
-                txtFieldNome.setText(Double.toString(calculo2));
-            }
+            txtFieldNome.setText(txtFieldNome.getText().toString() + "1");
         }
         if (e.getSource() == btn2) {
-            if (isValue2) {
-                calculo1 += 2;
-                txtFieldNome.setText(Double.toString(calculo1));
-            } else {
-                calculo2 += 2;
-                txtFieldNome.setText(Double.toString(calculo2));
-            }
+            txtFieldNome.setText(txtFieldNome.getText().toString() + "2");
         }
         if (e.getSource() == btn3) {
-            if (isValue2) {
-                calculo1 += 3;
-                txtFieldNome.setText(Double.toString(calculo1));
-            } else {
-                calculo2 += 3;
-                txtFieldNome.setText(Double.toString(calculo2));
-            }
+            txtFieldNome.setText(txtFieldNome.getText().toString() + "3");
         }
         if (e.getSource() == btn4) {
-            if (isValue2) {
-                calculo1 += 4;
-                txtFieldNome.setText(Double.toString(calculo1));
-            } else {
-                calculo2 += 4;
-                txtFieldNome.setText(Double.toString(calculo2));
-            }
+            txtFieldNome.setText(txtFieldNome.getText().toString() + "4");
         }
         if (e.getSource() == btn5) {
-            if (isValue2) {
-                calculo1 += 5;
-                txtFieldNome.setText(Double.toString(calculo1));
-            } else {
-                calculo2 += 5;
-                txtFieldNome.setText(Double.toString(calculo2));
-            }
+            txtFieldNome.setText(txtFieldNome.getText().toString() + "5");
         }
         if (e.getSource() == btn6) {
-            if (isValue2) {
-                calculo1 += 6;
-                txtFieldNome.setText(Double.toString(calculo1));
-            } else {
-                calculo2 += 6;
-                txtFieldNome.setText(Double.toString(calculo2));
-            }
+            txtFieldNome.setText(txtFieldNome.getText().toString() + "6");
         }
         if (e.getSource() == btn7) {
-            if (isValue2) {
-                calculo1 += 7;
-            } else {
-                calculo2 += 7;
-            }
+            txtFieldNome.setText(txtFieldNome.getText().toString() + "7");
         }
         if (e.getSource() == btn8) {
-            if (isValue2) {
-                calculo1 += 8;
-                txtFieldNome.setText(Double.toString(calculo1));
-            } else {
-                calculo2 += 8;
-                txtFieldNome.setText(Double.toString(calculo2));
-            }
+            txtFieldNome.setText(txtFieldNome.getText().toString() + "8");
         }
         if (e.getSource() == btn9) {
-            if (isValue2) {
-                calculo1 += 9;
-                txtFieldNome.setText(Double.toString(calculo1));
-            } else {
-                calculo2 += 9;
-                txtFieldNome.setText(Double.toString(calculo2));
-            }
+            txtFieldNome.setText(txtFieldNome.getText().toString() + "9");
+        }
+        //#endregion
 
-        }
-       
-        if(e.getSource() == menos){
-            operacao = "/";
-            isValue2 = true;
-        }
-        if(e.getSource() == mais){
-            operacao = "/";
-            isValue2 = true;
-        }
-        if(e.getSource() == multi){
-            operacao = "/";
-            isValue2 = true;
-        }
-        if(e.getSource() == dividir){
-            operacao = "/";
-            isValue2 = true;
-        }
-        if(e.getSource() == clear){
-            calculo1 = 0;
-            calculo1 = 0;
+
+        //#region Eventos Operacoes Aritmeticas
+        if (e.getSource() == menos) {
+            operacao = "-";
+            calculo1 = Double.parseDouble(txtFieldNome.getText());
             txtFieldNome.setText("");
-            isValue2 = false;
         }
+        if (e.getSource() == mais) {
+            operacao = "+";
+            calculo1 = Double.parseDouble(txtFieldNome.getText());
+            txtFieldNome.setText("");
+        }
+        if (e.getSource() == multi) {
+            operacao = "*";
+            calculo1 = Double.parseDouble(txtFieldNome.getText());
+            txtFieldNome.setText("");
+        }
+        if (e.getSource() == dividir) {
+            operacao = "/";
+            calculo1 = Double.parseDouble(txtFieldNome.getText());
+            txtFieldNome.setText("");
+        }
+        if (e.getSource() == clear) {
+            calculo1 = 0;
+            calculo2 = 0;
+            txtFieldNome.setText("");
+            menos.enable(); mais.enable(); dividir.enable(); multi.enable();
+        }
+        //#endregion
 
+        //#region Resultado
         if (e.getSource() == igual) {
-
             switch (operacao) {
                 case "-":
+                    calculo2 = Double.parseDouble(txtFieldNome.getText());
                     txtFieldNome.setText(Double.toString(calculo1 - calculo2));
-                    calculo1 = calculo1 - calculo2;
-                    calculo2 = 0;
-                    isValue2 = true;
+                    menos.disable(); mais.disable(); dividir.disable(); multi.disable();
                     break;
 
                 case "+":
+                    calculo2 = Double.parseDouble(txtFieldNome.getText());
                     txtFieldNome.setText(Double.toString(calculo1 + calculo2));
-                    calculo1 = calculo1 + calculo2;
-                    calculo2 = 0;
-                    isValue2 = true;
+                    menos.disable(); mais.disable(); dividir.disable(); multi.disable();
                     break;
-                case "x":
+                case "*":
+                    calculo2 = Double.parseDouble(txtFieldNome.getText());
                     txtFieldNome.setText(Double.toString(calculo1 * calculo2));
-                    calculo1 = calculo1 * calculo2;
-                    calculo2 = 0;
-                    isValue2 = true;
+                    menos.disable(); mais.disable(); dividir.disable(); multi.disable();
                     break;
                 case "/":
+                    calculo2 = Double.parseDouble(txtFieldNome.getText());
                     txtFieldNome.setText(Double.toString(calculo1 / calculo2));
-                    calculo1 = calculo1 / calculo2;
-                    calculo2 = 0;
-                    isValue2 = true;
+                    menos.disable(); mais.disable(); dividir.disable(); multi.disable();
                     break;
             }
         }
-
+        //#endregion
     }
 
 }
