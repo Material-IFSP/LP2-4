bfe2d7abea39bdeb75d9f7abae5b679e3760ed18
Anderson Conceição <andersonconceiicao@gmail.com>
2021-01-14 21:12:07 -0300

    FrontEnd TP Final Feito

---
+++ TPFinal/src/TPF/TPFView.java
@@ -0,0 +1,114 @@
+package TPF;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class TPFView extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtNome;
+	private JTextField txtIdade;
+	private JTextField txtPeso;
+	private JTextField txtAltura;
+	private JTextField txtObjetivo;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TPFView frame = new TPFView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TPFView() {
+		setTitle("Cadatro de Usu\u00E1rio");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 343, 211);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setHorizontalAlignment(SwingConstants.LEFT);
+		btnLimpar.setBounds(10, 135, 78, 23);
+		contentPane.add(btnLimpar);
+		
+		JButton btnSair = new JButton("Sair");
+		btnSair.setBounds(257, 135, 59, 22);
+		contentPane.add(btnSair);
+		
+		txtNome = new JTextField();
+		txtNome.setBounds(63, 23, 253, 20);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
+		
+		JLabel lblNome = new JLabel("Nome");
+		lblNome.setBounds(10, 26, 46, 14);
+		contentPane.add(lblNome);
+		
+		txtIdade = new JTextField();
+		txtIdade.setBounds(273, 54, 43, 20);
+		contentPane.add(txtIdade);
+		txtIdade.setColumns(10);
+		
+		JLabel lblIdade = new JLabel("Idade");
+		lblIdade.setBounds(238, 57, 35, 14);
+		contentPane.add(lblIdade);
+		
+		JLabel lblPeso = new JLabel("Peso");
+		lblPeso.setBounds(11, 57, 45, 14);
+		contentPane.add(lblPeso);
+		
+		txtPeso = new JTextField();
+		txtPeso.setColumns(10);
+		txtPeso.setBounds(63, 54, 48, 20);
+		contentPane.add(txtPeso);
+		
+		JLabel lblAltura = new JLabel("Altura");
+		lblAltura.setBounds(121, 57, 35, 14);
+		contentPane.add(lblAltura);
+		
+		txtAltura = new JTextField();
+		txtAltura.setColumns(10);
+		txtAltura.setBounds(164, 54, 64, 20);
+		contentPane.add(txtAltura);
+		
+		JLabel lblObjetivo = new JLabel("Objetivo");
+		lblObjetivo.setBounds(11, 85, 52, 14);
+		contentPane.add(lblObjetivo);
+		
+		txtObjetivo = new JTextField();
+		txtObjetivo.setColumns(10);
+		txtObjetivo.setBounds(63, 85, 167, 20);
+		contentPane.add(txtObjetivo);
+		
+		JButton btnIncluir = new JButton("Incluir");
+		btnIncluir.setBounds(238, 85, 78, 22);
+		contentPane.add(btnIncluir);
+		
+		JButton btnMostrar = new JButton("Apresentar Dados");
+		btnMostrar.setBounds(98, 135, 149, 23);
+		contentPane.add(btnMostrar);
+	}
+}
